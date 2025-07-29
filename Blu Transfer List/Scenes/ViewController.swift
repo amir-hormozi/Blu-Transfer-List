565304c5c93ca32135f856d0f63262ef2ab361f6
Behtis <amir.hormozi.n@gmail.com>
2025-07-29 19:35:11 +0330

    project initialized and created

---
+++ Blu Transfer List/Scenes/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Blu Transfer List
+//
+//  Created by Amir Hormozi on 7/29/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+}
